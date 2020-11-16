17229db37ddd94ef5ead46f1d9a5af2f9f3f9156
jan1264 <jan1264@ws368-201-23.szut.uni-bremen.de>
2020-11-16 11:54:22 +0100

    geändert / nicht vollständig

--- LF1/src/vektoren/BinaerAddition.java
+++ LF1/src/vektoren/BinaerAddition.java
@@ -16,23 +16,28 @@ public class BinaerAddition {
 		int[] ergebnis = new int[zahl1.length];
 
 		for (int i = 0; i < ergebnis.length; i++) {
-			
-			if (zahl1[i] == 0 && zahl2[i] == 0) {
-				ergebnis[i] = 0;
-			}else if ((zahl1[i] == 1 && zahl2[i] == 0) || (zahl1[i] == 0 && zahl2[i] == 1)) {
-				ergebnis[i] = 1;
-			}else if (zahl1[i] == 1 && zahl2[i] == 1) {
-				ergebnis[i] = 0;
-				ergebnis[i + 1] = ergebnis [i + 1] + 1;
-			}
-		}
-		System.out.println("");
-		System.out.println("Ergebnis: ");
-		for (int i = 0; i < ergebnis.length; i++) {
-			
-		
-			System.out.println(ergebnis[i]);
-		}
-	}
 
+				if (zahl1[i] == 0 && zahl2[i] == 0) {
+					ergebnis[i] = 0;
+				}else if ((zahl1[i] == 1 && zahl2[i] == 0) || (zahl1[i] == 0 && zahl2[i] == 1)) {
+					ergebnis[i] = 1;
+				}else if (zahl1[i] == 1 && zahl2[i] == 1) {
+					ergebnis[i] = 0;
+					ergebnis[i + 1] = ergebnis [i + 1] + 1;
+				}
+				
+			}
+		if(ergebnis[ergebnis.length - 1] == 0) {
+			int [] ergebnis2 = new int[ergebnis.length + 1];
+			ergebnis2[ergebnis2.length] = 1;
+			
+			System.out.println("");
+			System.out.println("Ergebnis: ");
+			for (int j = 0; j < ergebnis.length; j++) {
+
+				System.out.println(ergebnis[j]);
+				System.out.println(ergebnis2[j]);
+			}
+		}			
+	}
 }
