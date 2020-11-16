81e62cf38302433a5e855737d5bdeb84056e9eec
jtwoh <jtwoh@DESKTOP-IJ41PMM>
2020-11-16 17:24:26 +0100

    aktualisierung Von PC zuhause für übertragung der Dateien

--- LF1/src/vektoren/BinaerAddition.java
+++ LF1/src/vektoren/BinaerAddition.java
@@ -7,37 +7,65 @@ import oop.IO;
 public class BinaerAddition {
 
 	public static void main(String[] args) throws IOException {
+		System.out.println("Wie viele Stellen haben die Binärzahlen maximal?");
+        int binLength = IO.readInteger();
 
-		System.out.println("Geben sie die erste Binärzahl ein");
-		int[] zahl1 = IO.readIntegerArray1DBinaer();
-		System.out.println("Geben sie nun die Zweite Binärzahl ein");
-		int[] zahl2 = IO.readIntegerArray1D(zahl1.length);
+        System.out.println("Gib die erste Zahl ziffernweise von links ein");
+        int[] bin1 = createBinArr(binLength);
 
-		int[] ergebnis = new int[zahl1.length];
+        System.out.println("Gib die Zweite Zahl ziffernweise von links ein");
+        int[] bin2 = createBinArr(binLength);
+        
+        int[] addArr = new int[bin1.length];
+        boolean uebertrag = false;
 
-		for (int i = 0; i < ergebnis.length; i++) {
+        //Rechnung
+        for(int i = bin1.length - 1; i >= 0; i--){
+            //Übertragsrechnung
+            if(uebertrag){
+                uebertrag = false;
+                if(bin1[i] == 0){
+                    bin1[i] = 1;
+                }
+                else if(bin2[i] == 0){
+                    bin2[i] = 1;
+                }
+                else{
+                    uebertrag = true;
+                }
+            }
 
-				if (zahl1[i] == 0 && zahl2[i] == 0) {
-					ergebnis[i] = 0;
-				}else if ((zahl1[i] == 1 && zahl2[i] == 0) || (zahl1[i] == 0 && zahl2[i] == 1)) {
-					ergebnis[i] = 1;
-				}else if (zahl1[i] == 1 && zahl2[i] == 1) {
-					ergebnis[i] = 0;
-					ergebnis[i + 1] = ergebnis [i + 1] + 1;
-				}
-				
-			}
-		if(ergebnis[ergebnis.length - 1] == 0) {
-			int [] ergebnis2 = new int[ergebnis.length + 1];
-			ergebnis2[ergebnis2.length] = 1;
-			
-			System.out.println("");
-			System.out.println("Ergebnis: ");
-			for (int j = 0; j < ergebnis.length; j++) {
+            //Rechnung Normal
+            if(bin1[i] == 0 && bin2[i] == 0){
+                addArr[i] = 0;
+            }
+            if(bin1[i] == 1 && bin2[i] == 0 || bin1[i] == 0 && bin2[i] == 1){
+                addArr[i] = 1;
+            }
+            if(bin1[i] == 1 && bin2[i] == 1){
+                addArr[i] = 0;
+                uebertrag = true;
+            }
+        }
 
-				System.out.println(ergebnis[j]);
-				System.out.println(ergebnis2[j]);
-			}
-		}			
+        //Ergebnisse Ausgeben
+        System.out.print("Ergebnis: ");
+        for(int i: addArr){
+            System.out.print(i);
+        }
+    }
+    private static int[] createBinArr(int arrLength){
+        arrLength++;
+        int[] binArr = new int[arrLength];
+
+        for (int i = arrLength - 1; i >= 0; i--) {
+            if (i == 0) {
+                binArr[i] = 0;
+            } else {
+                binArr[i] = IO.readInteger();
+            }
+        }
+
+        return binArr;
+    }
 	}
-}
