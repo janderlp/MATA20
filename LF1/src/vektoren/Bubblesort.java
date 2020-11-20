65e13f3a01b831c80939aa544e5ef0c49a52a327
jan1264 <jan1264@ws368-201-23.szut.uni-bremen.de>
2020-11-20 11:08:27 +0100

    xxx

--- LF1/src/vektoren/Bubblesort.java
+++ LF1/src/vektoren/Bubblesort.java
@@ -10,16 +10,22 @@ public class Bubblesort {
 
 	public static void main(String[] args) throws IOException {
 
-		System.out.print("Wieviele zahlen wollen sie eingeben ? :");
+		System.out.print("Wieviele zahlen wollen sie eingeben ? : ");
 		int laengezahlen = IO.readInteger();
-
+		
 		double[] zahlen = new double[laengezahlen];
+		
+		for(int i = 0; i < laengezahlen; i++) {
+			zahlen[i] = Math.random() * 165762;
+		}
+		
+		/*double[] zahlen = new double[laengezahlen];
 
 		System.out.println(" ");
 		System.out.println("geben sie nun die zahlen ein : ");
 		for (int i = 0; i < zahlen.length; i++) {
 			zahlen[i] = IO.readInteger();
-		}
+		}*/
 
 		for (int j = 0; j < laengezahlen + 1; j++) {
 
@@ -35,9 +41,11 @@ public class Bubblesort {
 
 			}
 		}
+		
+		
 		System.out.println("Zahlen in aufsteigender Reihenfolge : ");
 		for (int i = 0; i < zahlen.length; i++) {
-			System.out.print(zahlen[i] + "; ");
+			System.out.println( String.format("%.2f", zahlen[i]) + "; ");
 		}
 	}
 }
