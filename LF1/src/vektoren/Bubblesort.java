7cd30c53fac1e63ec6fb150a6d740e1db35b7cdd
jan1264 <jan1264@ws368-201-23.szut.uni-bremen.de>
2020-11-20 10:40:22 +0100

    Bubblesort fertiggestelt

--- LF1/src/vektoren/Bubblesort.java
+++ LF1/src/vektoren/Bubblesort.java
@@ -1,7 +1,7 @@
 package vektoren;
 
 import java.io.IOException;
-import java.util.Arrays;
+
 
 import oop.IO;
 
@@ -9,20 +9,35 @@ public class Bubblesort {
 
 
 	public static void main(String[] args) throws IOException {
-		
+
 		System.out.print("Wieviele zahlen wollen sie eingeben ? :");
 		int laengezahlen = IO.readInteger();
-		
+
 		int [] zahlen = new int [laengezahlen];
-		
+
 		System.out.println(" ");
 		System.out.println("geben sie nun die zahlen ein : ");
 		for (int i = 0; i < zahlen.length; i++) {
 			zahlen[i] = IO.readInteger();
 		}
-		Arrays.sort(zahlen);
-		System.out.println(Arrays.toString(zahlen));
-		
+
+		for (int j = 0; j < laengezahlen + 1; j++) {
+
+
+			for (int i = 0; i < zahlen.length - 1; i++) {
+				if (zahlen[i] > zahlen[i + 1]) {
+					int c = zahlen[i];
+					zahlen[i] = zahlen[i + 1];
+					zahlen[i + 1] = c;
+
+
+				}
+
+			}
+		}
+		System.out.println("Zahlen in der Reihenfolge : ");
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.print(zahlen[i] + "; ");
+		}
 	}
 }
-
