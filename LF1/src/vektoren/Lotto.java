fb6e2620cfd2b93fb96187297966129c4c97eacf
jan1264 <jan1264@ws368-201-23.szut.uni-bremen.de>
2020-11-23 10:25:45 +0100

    Lotto/ vorläufig

---
+++ LF1/src/vektoren/Lotto.java
@@ -0,0 +1,88 @@
+package vektoren;
+
+public class Lotto {
+
+	public static void main(String[] args) {
+
+		ohnezurueck();
+		mitzurueck();
+
+	}
+
+	public static void ohnezurueck() {
+		int[] lotto = new int[7];
+		boolean wahl = false;
+		System.out.println("die gezogenen Lottozahlen + Zusatzzahl sind: (ohne zurücklegen)");
+		for (int i = 0; i < lotto.length; i++) {
+			lotto[i] = (int) (Math.random() * 49);
+
+			while(lotto[i] == 0 || lotto[i] > 49 || wahl == false){
+				lotto[i] = (int) (Math.random() * 49);
+				if(lotto[i] != 0 || lotto[i] <= 49) {
+					wahl = true;
+				}
+			} 
+		}
+		sortieren(lotto);
+		for (int i = 0; i < lotto.length - 1; i++) {
+			System.out.print(lotto[i] + ", ");
+		}
+
+
+		lotto[6] = (int) (Math.random() * 49);
+		System.out.println("");
+		System.out.println(lotto[6]);
+	}
+
+	private static int[] sortieren(int[] lotto) {
+		for (int j = 0; j < lotto.length - 1; j++) {
+
+			for (int i2 = 0; i2 < lotto.length - 1; i2++) {
+				if (lotto[i2] > lotto[i2 + 1]) {
+					int c = lotto[i2];
+					lotto[i2] = lotto[i2 + 1];
+					lotto[i2 + 1] = c;
+				}
+
+			}
+		}
+		return lotto;
+	}
+
+	public static void mitzurueck() {
+		int[] lotto = new int[7];
+		boolean wahl = false;
+		System.out.println("die gezogenen Lottozahlen + Zusatzzahl sind: (mit zurücklegen)");
+		for (int i = 0; i < lotto.length; i++) {
+			lotto[i] = (int) (Math.random() * 49);
+
+			while(lotto[i] == 0 || lotto[i] > 49 || wahl == false){
+				lotto[i] = (int) (Math.random() * 49);
+				if(lotto[i] != 0 || lotto[i] <= 49) {
+					wahl = true;
+				}
+			} 
+		}
+		sortieren(lotto);
+		for (int i = 0; i < lotto.length - 1; i++) {
+			System.out.print(lotto[i] + ", ");
+		}
+
+		lotto[6] = (int) (Math.random() * 49);
+		System.out.println("");
+				while(lotto[6] == 0 || lotto[6] > 49 || wahl == false){
+				lotto[6] = (int) (Math.random() * 49);
+				if(lotto[6] != 0 || lotto[6] <= 49) {
+					wahl = true;
+				}
+			} 
+
+			System.out.print(lotto[lotto.length - 1]);
+			System.out.println("");
+
+
+
+		}
+
+	}
+
